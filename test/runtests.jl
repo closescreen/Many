076685ba1afd44f3d6e47f973ab0207ba097d28c
closescreen <close.screen@gmail.com>
2016-10-12 17:04:26 +0300

    fill tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using Many
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@test collect( Many.hours(DateTime(2016,04,04,15), 3)) |> length == 3
+@test collect( Many.hours(DateTime(2016,04,04,15), Dates.Hour(3))) |> length == 3
+@test first( collect( Many.hours(DateTime(2016,04,04,15), Dates.Hour(3))) ) == DateTime(2016,4,4,15)
+@test last( collect(Many.hours(DateTime(2016,04,04,15), Dates.Day(1))))==DateTime(2016,4,5,14)
+@test last(collect(Many.hours(Date(2016,04,04), Dates.Day(1)))) == DateTime(2016,4,4,23)
